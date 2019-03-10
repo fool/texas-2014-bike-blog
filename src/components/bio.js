f60dc5432b2dc9f2b0895a47ac690a8ed9652c2a
fool <chrism@netlify.com>
2019-03-09 16:17:43 -0800

    removed Kyle, added me

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,11 +38,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by 
+              <a href={`https://twitter.com/${social.twitter}`}><strong>{author}</strong></a>
+              who lives and plays in Portland, Oregon. 
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
               </a>
             </p>
           </div>
